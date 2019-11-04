99f5de041def31053f4afe41e484875952896848
zwimer <zwimer@gmail.com>
2019-11-03 18:17:33 -0700

    Shows all repos now

--- ls-repos.sh
+++ ls-repos.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -eux
 
 # Setup
+export NUM_RESULTS=999999
 . ./setup.sh
 
 # ls
-export INFO=$(curl_reg "${REG_URI}/${VERSION}/_catalog")
+export INFO=$(curl_reg "${REG_URI}/${VERSION}/_catalog?n=${NUM_RESULTS}")
 echo "${INFO}" | python -m json.tool
